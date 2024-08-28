02752d062c2980c9a239f55d0fdf2afa1704466e
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2024-08-28 21:44:45 +0530

    changes 0.0.2

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,13 +14,13 @@ const Sidebar = () => {
 
     return (
         <div className='flex flex-col w-full '>
-            <div className='px-4 mb-4 flex items-center justify-between pb-3'><span className={`${rajdhani.className} font-bold text-[32px]`}>Aelomotion</span><ModeToggle /></div>
+            <div className='px-4 mb-4 flex items-center justify-between pb-3'><span className={`${rajdhani.className} font-bold text-[32px] `}>Aelomotion</span><ModeToggle /></div>
             {SidebarContent.map((data, key) => (
-                <div key={key} className='text-start px-4'>
-                    <Link href={data.href} onClick={() => setId(data.ID)} className={`w-full text-[16px] line-clamp-2 font-semibold flex items-center gap-2 ${data.ID === id ? 'opacity-100' : 'opacity-80'}`}><Boxes size={18} />{data.name}</Link>
+                <div key={key} className='text-start '>
+                    <Link href={data.href} onClick={() => setId(data.ID)} className={`w-full text-[16px] line-clamp-2 font-medium flex items-center gap-2 px-4 ${data.ID === id ? 'opacity-100' : 'opacity-80'}`}><Boxes size={18} />{data.name}</Link>
                     <div className='flex flex-col text-[12px] opacity-80 mt-4 '>
                         {data.contents.map((data, key) => (
-                            <Link key={key} href={data.href} onClick={() => setId(data.ID)} className={`w-full py-3 box-border px-4 rounded-sm hover:bg-black ${path === data.href && 'bg-black'} flex items-center gap-2`}><CornerDownRight size={18} />{data.name}</Link>
+                            <Link key={key} href={data.href} onClick={() => setId(data.ID)} className={`w-full py-3 box-border px-4  hover:bg-black ${path === data.href && 'bg-black'} flex items-center gap-2`}><CornerDownRight size={18} />{data.name}</Link>
                         ))}
                     </div>
                 </div>
