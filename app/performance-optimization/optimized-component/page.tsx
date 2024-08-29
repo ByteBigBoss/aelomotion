53c426ae027dbbf357e29c34c8a77f7758ccecee
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2024-08-29 05:55:06 +0530

    changes 0.0.6

--- app/performance-optimization/optimized-component/page.tsx
+++ app/performance-optimization/optimized-component/page.tsx
@@ -37,11 +37,11 @@ const PerformanceOptimization = () => {
         <div className="text-[2rem] font-bold">Why Use will-change?</div>
         <div>
           <div className="text-[1.25rem] font-bold">Performance Optimization</div>
-          <span>By using will-change, the browser knows in advance which properties are going to change and can prepare by creating a separate layer for that element, reducing the chances of layout shifts and rendering issues.</span>
+          <span className="opacity-80">By using will-change, the browser knows in advance which properties are going to change and can prepare by creating a separate layer for that element, reducing the chances of layout shifts and rendering issues.</span>
         </div>
         <div>
           <div className="text-[1.25rem] font-bold">Less Jank</div>
-          <span>Especially important for animations that might cause the browser to re-render or reflow elements on the page, reducing visual glitches during the animation.</span>
+          <span className="opacity-80">Especially important for animations that might cause the browser to re-render or reflow elements on the page, reducing visual glitches during the animation.</span>
         </div>
       </div>
     </div>
